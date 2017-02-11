033a19c5d116fc9d26ccbfea2c130ef0b780ba10
Nathan McGinn <odstderek@gmail.com>
2017-02-11 13:38:29 -0600

    json

--- all_test.go
+++ all_test.go
@@ -4,6 +4,13 @@ import (
 	"testing"
 )
 
+func TestJson(t *testing.T) {
+	str := ReadJson()
+	if str != "somedata" {
+		t.Error("JSON did not read correctly.")
+	}
+}
+
 func TestYaml(t *testing.T) {
 	str := ReadYaml()
 	if str != "somedata" {
